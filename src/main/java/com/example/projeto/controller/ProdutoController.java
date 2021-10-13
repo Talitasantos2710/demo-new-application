eac3dd12209b245bd7f69ba63210c1f5e983aa8e
Talita <talita.santoos27@gmail.com>
2021-10-12 22:53:08 -0300

    alterei produto controller

--- src/main/java/com/example/projeto/controller/ProdutoController.java
+++ src/main/java/com/example/projeto/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package com.example.projeto.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +19,28 @@ public class ProdutoController {
 
 	@Autowired
 	public ProdutoRepository produtorepository;
-	
+
 	@GetMapping
-	public Iterable<Produto> getProdutos(){
-	   return	produtorepository.findAll();
+	public Iterable<Produto> getProdutos() {
+		return produtorepository.findAll();
+
 	}
-	
+
+	@PostMapping
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtorepository.save(produto);
+
+	}
+
+	@DeleteMapping
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtorepository.delete(produto);
+
+	}
+
+	@PutMapping
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtorepository.save(produto);
+	}
+
 }
